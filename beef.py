5e7b5df076a46794bc3c58d4af510df2dd11da89
XMPPwocky <nttheis@gmail.com>
2016-05-04 14:24:46 -0700

    Hilariously huge optimization (>32000x speedup)
    Bruteforce each 32-bit LCG state separately.
    We know 16 bits of each state, so we need to bruteforce 16 bits twice:
    17 bits.
    Before, we bruteforced the 32 bits we didn't know together- doing 2^15
    times more work!

--- beef.py
+++ beef.py
@@ -1,10 +1,11 @@
 import pyopencl as cl
+import itertools
 import sys
 import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("rng_type", choices=["18030", "18273"])
+parser.add_argument("a", type=int) 
 parser.add_argument("outputs", nargs="+", type=float)
 parser.add_argument("--cl-platform", default=0, type=int)
 parser.add_argument("--cl-device", default=0, type=int)
@@ -18,23 +19,14 @@ device = platform.get_devices()[args.cl_device]
 ctx = cl.Context([device])
 queue = cl.CommandQueue(ctx)
 
-if args.rng_type == "18030":
-    kernel_src = open("18030.cl").read()
-elif args.rng_type == "18273":
-    kernel_src = open("18273.cl").read()
+kernel_src=open("generic.cl").read()
 
 program = cl.Program(ctx, kernel_src).build()
 
 max_results = args.max_results 
 mem_flags = cl.mem_flags
-result_np = np.zeros(max_results, np.uint32)
-result_cl = cl.Buffer(ctx, mem_flags.WRITE_ONLY, result_np.nbytes);
 
 queue = cl.CommandQueue(ctx)
-search_len_np =  np.array(args.max_skip, dtype=np.uint32)
-result_count_np =  np.array(0, dtype=np.uint32)
-result_count_cl = cl.Buffer(ctx, mem_flags.READ_WRITE, result_count_np.nbytes);
-result_max_np =  np.array(max_results, dtype=np.uint32)
 
 def normalize(n):
     return float(int(n * (1<<32))) / (1<<32)
@@ -42,38 +34,53 @@ def normalize(n):
 def toint(n):
     return int(n / np.float64(2.3283064365386962890625e-10))
 
+uint32_size = 4
+
 full_outputs = map(toint, map(normalize, args.outputs))
-state_sample = full_outputs[0] 
 
-lower_state_np = np.array([state_sample>>16, state_sample&0xFFFF], dtype=np.uint32)
+def core_search(partial_outputs, a):
+        search_len_np = np.array(args.max_skip, dtype=np.uint32)
+        result_count_cl = cl.Buffer(ctx, mem_flags.READ_WRITE, uint32_size)
+        result_cl = cl.Buffer(ctx, mem_flags.WRITE_ONLY, max_results*uint32_size) 
+        cl.enqueue_fill_buffer(queue, result_count_cl, "\x00", 0, uint32_size)
+        result_max_np = np.array(max_results, dtype=np.uint32)
+        a_np = np.array(a, dtype=np.uint32)
+        outputs_np = np.array(partial_outputs, dtype=np.uint32)
+        outputs_cl = cl.Buffer(ctx, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=outputs_np) 
+        outputs_len_np = np.array(len(outputs_np), dtype=np.uint32)
 
-outputs_np = np.array(full_outputs[1:], dtype=np.uint32)
-outputs_cl =  cl.Buffer(ctx, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=outputs_np) 
+        program.node_newer_rng(queue, [1<<16], None, search_len_np, result_max_np, a_np, outputs_cl, outputs_len_np, result_count_cl, result_cl)
 
-outputs_len_np = np.array(len(outputs_np), dtype=np.uint32)
-print "Starting search..."
-program.node_newer_rng(queue, [(1<<32) - 1], None, lower_state_np, search_len_np, result_max_np, outputs_cl, outputs_len_np, result_count_cl, result_cl)
+        return result_count_cl, result_cl
 
-cl.enqueue_copy(queue, result_count_np, result_count_cl)
-cl.enqueue_copy(queue, result_np, result_cl)
-print "Found", result_count_np, "results."
+print "Starting search for r0"
+(result_count_r0_cl, result_r0_cl) = core_search(map(lambda x: ((x >> 16)&0xFFFF), full_outputs), args.a)
+print "Starting search for r1"
+(result_count_r1_cl, result_r1_cl) = core_search(map(lambda x: ((x >> 16)&0xFFFF), full_outputs), args.a)
+
+result_count_r0_np = np.array(0, dtype=np.uint32)
+result_r0_np = np.zeros(max_results, np.uint32)
+result_count_r1_np = np.array(0, dtype=np.uint32)
+result_r1_np = np.zeros(max_results, np.uint32)
+
+cl.enqueue_copy(queue, result_count_r0_np, result_count_r0_cl)
+cl.enqueue_copy(queue, result_r0_np, result_r0_cl)
+cl.enqueue_copy(queue, result_count_r1_np, result_count_r1_cl)
+cl.enqueue_copy(queue, result_r1_np, result_r1_cl)
+
 np.set_printoptions(precision=32)
-for q in result_np[0:result_count_np]:
-    j = np.uint32(lower_state_np[0] | ((q & 0xFFFF)<<16))
-    k = np.uint32(lower_state_np[1] | (q & 0xFFFF0000))
-    #(j, k) = (939816633, 375672958)
 
-
+print "Found", result_count_r0_np, "results for r0."
+print "Found", result_count_r1_np, "results for r1."
+results = itertools.product(
+        result_r0_np[0:result_count_r0_np],
+        result_r1_np[0:result_count_r1_np]
+        )
+for j, k in results: 
     print "\tState: (" + str(j) + ", "+ str(k) + ")"
     for i in range(args.dump_length):
-        if args.rng_type == "18273":
-            j = (18273 * (j & 0xFFFF)) + (j >> 16)
-            k = (36969 * (k & 0xFFFF)) + (k >> 16)
-            x = np.uint32((j << 16) + (k & 0xFFFF))
-            pred = x * np.float64("2.3283064365386962890625e-10")
-        if args.rng_type == "18030":
-            j = (18030 * (j & 0xFFFF)) + (j >> 16)
-            k = (36969 * (k & 0xFFFF)) + (k >> 16)
-            x = np.uint32((j << 16) + (k & 0xFFFF))
-            pred = x * np.float64("2.3283064365386962890625e-10")
+        j = (args.a * (j & 0xFFFF)) + (j >> 16)
+        k = (36969 * (k & 0xFFFF)) + (k >> 16)
+        x = np.uint32((j << 16) + (k & 0xFFFF))
+        pred = x * np.float64("2.3283064365386962890625e-10")
         print "\t\t" + ("%.20f" % normalize(pred))
---
+++ generic.cl
@@ -0,0 +1,35 @@
+uint run_rng(uint state, uint a) {
+    return (a * (state & 0xFFFF)) + (state >> 16);
+}
+
+__kernel void node_newer_rng(
+    const uint search_len,
+    const uint successful_guesses_max,
+    const uint a,
+
+    __global const uint *outputs,
+    const uint outputs_len,
+    __global uint *successful_guesses_count,
+    __global uint *successful_guesses
+) {
+    uint gid = get_global_id(0);
+    uint guess = gid;
+
+    uint state = outputs[0] | (guess<<16);
+
+    uint cur_output = 1;
+    for (uint i = 0; i <= search_len && cur_output < outputs_len; i++) {
+        *&state = run_rng(state, a);
+        if ((state&0xFFFF) == outputs[cur_output]) {
+            cur_output += 1;
+            i = 0;
+        }
+    }
+
+    if (cur_output == outputs_len) {
+        uint guess_idx = atomic_inc(successful_guesses_count);
+        if (guess_idx < successful_guesses_max) {
+            successful_guesses[guess_idx] = state;
+        }
+    }
+}
